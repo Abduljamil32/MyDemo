b4d588a215c65f33fafc4f5dacacbe74f8753f31
co.abu <anizomxoja@gmail.com>
2022-04-12 15:32:38 +0500

    Initial Commit

---
+++ MyDemo/MyDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyDemoApp.swift
+//  MyDemo
+//
+//  Created by Avaz Mukhitdinov on 12/04/22.
+//
+
+import SwiftUI
+
+@main
+struct MyDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
